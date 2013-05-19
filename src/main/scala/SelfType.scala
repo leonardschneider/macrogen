d014d5e0fde2f6f46df6c3eb6d84dded88c5f3e6
Leonard Schneider <leonard.schneider@gmail.com>
2013-05-19 13:28:20 +0200

    Fixed #1

--- src/main/scala/SelfType.scala
+++ src/main/scala/SelfType.scala
@@ -9,8 +9,15 @@ object SelfType {
     import c.universe._
 
     val Template(parents, self, defs) = c.enclosingTemplate
-    val tpe = Ident(c.enclosingImpl.name)
-    val typedef = q"type Self = $tpe"
+    val typedef = c.enclosingImpl match {
+      case ClassDef(_, name, tparams, _) if tparams.length > 0 =>
+        val targs = tparams.map(t => tq"${t.name}")
+        q"type Self[..$tparams] = $name[..$targs]"
+      case ClassDef(_, name, _, _) =>
+        q"type Self = $name"
+      case ModuleDef(_, name, _) =>
+        q"type Self = $name"
+    }
     val res = Template(
       parents.filter(t => !t.equalsStructure(tq"SelfType")),
       self,
